9f0a499f1980e1182e4e8345c7c6bb980c8aaf01
omartnc <omer_tunc_04@hotmail.com>
2019-01-29 21:38:01 +0300

    change project

--- hash.js
+++
@@ -1,3 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function run() {}
--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const Joi = require("joi");
-const config = require("config");
-Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
-
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const companies = require("./routes/companies");
-
-const express = require("express");
+const winston = require('winston');
+const express = require('express');
 const app = express();
 
-if (!config.get("jwtPrivateKey")) {
-  console.log("FATAL EROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+require('./startup/logging');
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
-mongoose
-  .connect("mongodb://localhost/WhereIsTheMyServiceDb")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use("/api/companies", companies);
-
-const port = process.env.PORT || 1000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
