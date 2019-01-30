01eb2af69f818bbb8d3f3d9cbe246bd238d2fa64
Ömer TUNÇ <omer_tunc_04@hotmail.com>
2019-01-30 11:54:15 +0300

    update general

--- routes/locations.js
+++ routes/locations.js
@@ -23,7 +23,7 @@ router.get("/:id", [auth], async (req, res) => {
   res.send(location);
 });
 
-router.post("/", auth, async (req, res) => {
+router.post("/", auth, async (req, res) => { 
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
